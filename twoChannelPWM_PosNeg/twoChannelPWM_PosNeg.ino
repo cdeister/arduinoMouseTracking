183c86704667340dc19d9ca4f3472ce3d9ab3e13
Chris Deister <cdeister@brown.edu>
2017-05-09 21:35:28 -0400

    1.1 - More Goodies
    I added a simple Matlab program that will let you connect to an
    arudio’s com port and collect delta x, accumulated position and time
    stamps from the arduio. It plots a small stretch of delta x values in
    realish time.

--- twoChannelPWM_PosNeg/twoChannelPWM_PosNeg.ino
+++ twoChannelPWM_PosNeg/twoChannelPWM_PosNeg.ino
@@ -1,10 +1,25 @@
 /*
    See how they run
    Three blind mice
-   simple pwm output from a host shield over serial
-   for one axis you should stick to pins 5 and 6 for an uno
-    13 and 4 are timer 0 for mega 2560
-   they are controlled by timer 0 and by default give:
+   simple pwm output from a host shield 
+   also position values etc over serial if that's your bag
+
+   This file defaults to a mega config with output pins:
+   4 and 13, but if you use an uno change to 5 and 6.
+
+   If you are using serial:
+   I also added a reset pin at 7, which will reset
+   time and current position to 0. This is an easy way to mark
+   trials etc. You can have your PFI0 from a nidaq board, or some 
+   output from pClamp reset when you start a run. 
+   Again, this only matters for serial streaming of those variables.
+   
+   Nerdy Timer Info (for PWM):
+   for one axis you should stick to pins:
+   5 and 6 for an uno
+   13 and 4 for mega 2560
+   These are the faster timer 0 for each board
+   By default timer 0 gives:
    fPWM=976.563 Hz; with cycle length of 256; 70.7% duty (3.53V)
 
    questions --> cdeister@brown.edu
@@ -24,10 +39,20 @@
 
 const int posX_aOut = 4;
 const int negX_aOut = 13;
+const int resetPin = 7;
 
+int resetState;
 
-int deltaX = 0;
-//long posX=0;
+signed long tt;
+signed long tB;
+
+long curPos=0;
+long deltaX = 0;
+long tempDe = 0;
+long archPos=0;
+int output_posXValue;
+int output_negXValue;
+
 
 
 // ********** mouse class
@@ -57,29 +82,58 @@ void setup() {
   while (!Serial);    // wait for the serial port to open
   if (Usb.Init() == -1)
     Serial.println("OSC did not start.");
-  Serial.println("Start");
+  // Serial.println("Start");
   delay(500);
   HidMouse.SetReportParser(0, (HIDReportParser*)&Prs);
+  tB=millis();
 }
 
 void loop() {
   Usb.Task();
-    if(deltaX>=0){
-      int output_posXValue = map(deltaX, 0, 127, 0, 255);
-      int output_negXValue = 0;
-      analogWrite(posX_aOut,output_posXValue);
-      analogWrite(negX_aOut,output_negXValue);
-    }
-    else if(deltaX<0){
-      int output_negXValue = map(deltaX, -1, -128, 0, 255);
-      int output_posXValue = 0;
-      analogWrite(posX_aOut,output_posXValue);
-      analogWrite(negX_aOut,output_negXValue);
-    };
+  resetState=digitalRead(resetPin);
   
-  Serial.println(deltaX + 128);
+  if(resetState){
+    tB=micros();
+    curPos=0;
+  };
+  
+  tt=(micros()-tB)*0.0001;
+  if(deltaX>0){
+    output_posXValue = map(deltaX, 1, 127, 0, 255);
+    output_negXValue = 0;
+    analogWrite(posX_aOut,output_posXValue);
+    analogWrite(negX_aOut,output_negXValue);
+    //curPos=curPos;
+  }
+  else if(deltaX<0){
+    output_negXValue = map(deltaX, -1, -127, 0, 255);
+    output_posXValue = 0;
+    analogWrite(posX_aOut,output_posXValue);
+    analogWrite(negX_aOut,output_negXValue);
+    //curPos=curPos;
+  }
+  else if(deltaX==0){
+    output_negXValue = 0;
+    output_posXValue = 0;
+    analogWrite(posX_aOut,output_posXValue);
+    analogWrite(negX_aOut,output_negXValue);
+    //curPos=curPos;
+  };
 
-  delayMicroseconds(500);
+  curPos=curPos+deltaX;
+  Serial.print("posData");
+  Serial.print(',');
+  Serial.print(tt);
+  Serial.print(',');
+  Serial.print(deltaX);
+  Serial.print(',');
+  Serial.print(curPos);
+  Serial.print(',');
+  Serial.print(output_posXValue);
+  Serial.print(',');
+  Serial.println(output_negXValue);
+  
+  delayMicroseconds(50);
   deltaX = 0;
 
 
